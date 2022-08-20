8113beedbe721dab310683ce9b923cdefc5a1380
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-20 11:05:13 -0600

    Added some basic copy as well as our soon-to-be iconic color-changing logo.

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import FrfLogo from "./lib/FRFLogo.svelte";
+</script>
+
+<main>
+  <nav>
+    <FrfLogo />
+  </nav>
+  <section class="main-section">
+    <p>Front Range Foundry is dedicated to three pursuits:</p>
+    <ol>
+      <li>Build Cool Stuff</li>
+      <li>Build With Cool People</li>
+      <li>Write Cool Articles.</li>
+    </ol>
+
+    <h2>FAQ</h2>
+    <p><b>Can I build anything for FRF?</b></p>
+    <p>Within reasonable bounds of ethics and feasibility.</p>
+    <hr>
+    <p><b>Why Write articles?</b></p>
+    <p>The web is, at its heart, a collection of linked information. While it's run entirely on code, most of this code is useless to the casual observer unless its reason for being and method of construction is well documented. With articles, FRF writers can capture the key values of the code they produce.</p>
+    <hr>
+    <p><b>Does FRF have meetups?</b></p>
+    <p>Not as such.</p>
+    <p><b>What are FRF's current projects?</b></p>
+    <p>We're so glad you asked.</p>
+  </section>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/FRFLogo.svelte
@@ -0,0 +1,64 @@
+<script>
+  function randomColorPalette() {
+    const colors = {
+      "Clown Makeup Red": "#ff1616",
+      "Nice Restaurant Salmon": "#ff5757",
+      "The Pink Playdough": "#ff66c4",
+      "Barney the Dinosaur": "#cb6ce6",
+      "Imperial Rome": "#8c52ff",
+      "The Purr Pulls": "#5e17eb",
+      "Improbable Water Teal": "#03989e",
+      "Is this Cyan?": "#00c2cb",
+      "Light Blue": "#5ce1e6",
+      "Another Light Blue": "#38b6ff",
+      "Yet Another Light Blue": "#5271ff",
+      "Truly Dark Blue": "#004aad",
+      "Tree Green": "#008037",
+      "Grass Green": "#7ed957",
+      "Sickly Green": "#c9e265",
+      "French Fry Yellow": "#ffde59",
+      "Nice Restaurant French Fry Yellow": "#ffbd59",
+      "It's Just Orange": "#ff914d"
+    }
+
+    const keys = Object.keys(colors);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    const baseHex = colors[randomKey];
+
+    let slightlyDarker = shadeBy(baseHex, 25);
+    let slightlyLighter = shadeBy(baseHex, -25);
+
+    return {
+      f: slightlyDarker,
+      r: baseHex,
+      secondF: slightlyLighter
+    }
+  }
+
+  function shadeBy(color, amount) {
+    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+  }
+
+  const palette = randomColorPalette();
+</script>
+
+<div>
+  <p class="logo-letters">
+    <span id="first-f" style="color: {palette.f}">F</span><span id="the-r" style="color: {palette.r}">R</span><span id="second-f" style="color: {palette.secondF}">F</span>
+  </p>
+</div>
+
+<style>
+  .logo-letters {
+    font-size: 2rem;
+    font-weight: bold;
+    font-style: italic;
+    margin: 0.5rem;
+    margin-left: 1.75rem;
+    font-family: 'impact', sans-serif;
+  }
+
+  span {
+    margin-right: -1.75rem;
+  }
+</style>
